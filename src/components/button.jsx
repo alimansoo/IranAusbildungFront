d2d053f0b90531cb026bed4ae68a64bbce0f7409
Ali Mansoori <71168717+alimansoo@users.noreply.github.com>
2024-01-27 10:44:20 +0330

    change button

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -112,7 +112,7 @@ function ContentBtn({ children, rightIcon, leftIcon, loading }) {
     <>
       {loading !== undefined && loading ? (
         <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center">
-          <LoadingScreen />
+          Loading...
         </div>
       ) : (
         ''
